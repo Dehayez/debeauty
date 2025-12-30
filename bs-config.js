4e8a60ce46d16c491240ee7701a20b5d7e9101a5
Dehayez <yentel.dh@hotmail.be>
2025-12-30 17:19:30 +0100

    Remove .html extensions from URLs for clean routing
    - Update netlify.toml to redirect .html URLs to clean URLs and rewrite clean URLs to .html files
    - Update bs-config.js to handle root path and all routes without .html extensions
    - Remove .html extensions from all internal links in HTML files
    - URLs now work without .html extensions on both localhost and production

--- bs-config.js
+++ bs-config.js
@@ -2,14 +2,16 @@ module.exports = {
   server: {
     baseDir: "./",
     middleware: function (req, res, next) {
+      const url = req.url.split('?')[0];
       const cleanUrlMap = {
+        '/': '/index.html',
         '/diensten': '/diensten.html',
         '/over': '/over.html',
         '/contact': '/contact.html'
       };
       
-      if (cleanUrlMap[req.url]) {
-        req.url = cleanUrlMap[req.url];
+      if (cleanUrlMap[url]) {
+        req.url = cleanUrlMap[url] + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
       }
       
       next();
